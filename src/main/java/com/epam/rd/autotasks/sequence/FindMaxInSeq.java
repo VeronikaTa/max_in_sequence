79b363bf36379c08da799eae526637ad6a1005e1
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-09-21 14:17:23 +0300

    edited max()

--- src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
+++ src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
@@ -1,19 +1,36 @@
 package com.epam.rd.autotasks.sequence;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FindMaxInSeq {
     public static int max() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        boolean addInt = true;
+        do{
+            int number;
+            number = scanner.nextInt();
+            if(number != 0){
+                numbers.add(number);
+            }
+            else {
+                addInt = false;
+            }
+        }
+        while(addInt);
 
-        // Put your code here
-
-        return 0;
+        int max = numbers.get(0);
+        for(Integer item : numbers){
+            if(item > max){
+                max = item;
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
 
         System.out.println("Test your code here!\n");
-
-        // Get a result of your code
 
         System.out.println(max());
     }
